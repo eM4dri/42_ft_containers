aab957b4450cea233ea970f41a14d9a4d3a425ae
eM4dri <edu.mail.delivery+github@gmail.com>
2022-06-27 20:51:36 +0200

    enable_if

---
+++ includes/enable_if.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   enable_if.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 20:51:21 by emadriga          #+#    #+#             */
+/*   Updated: 2022/06/27 20:51:22 by emadriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+namespace ft{
+	template<bool Cond, class T = void> 
+	struct enable_if {};
+
+	template<class T>
+	struct enable_if<true, T> 
+	{	
+		typedef T type; 
+	};
+
+}
