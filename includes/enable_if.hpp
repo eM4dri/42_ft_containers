0b139b1d004d919eb96d3fecb602be4b45c09b0e
eM4dri <edu.mail.delivery+github@gmail.com>
2022-06-27 21:12:20 +0200

    enable_if + is_integral

--- includes/enable_if.hpp
+++ includes/enable_if.hpp
@@ -6,13 +6,14 @@
 /*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 20:51:21 by emadriga          #+#    #+#             */
-/*   Updated: 2022/06/27 20:51:22 by emadriga         ###   ########.fr       */
+/*   Updated: 2022/06/27 20:56:47 by emadriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-namespace ft{
+namespace ft
+{
 	template<bool Cond, class T = void> 
 	struct enable_if {};
 
---
+++ includes/is_integral.hpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_integral.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 20:56:13 by emadriga          #+#    #+#             */
+/*   Updated: 2022/06/27 21:11:33 by emadriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once 
+
+namespace ft
+{
+	template <class T>
+	struct	is_integral
+		{	static const bool m_Value = false;	};
+
+	template<>
+	struct is_integral<bool> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<char> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<char16_t> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<char32_t> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<wchar_t> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<signed char> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<short int> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<int> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<long int> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<long long int> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<unsigned char> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<unsigned short int> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<unsigned int> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<unsigned long int> 
+		{	static const bool m_Value = true;	};
+		
+	template<>
+	struct is_integral<unsigned long long int> 
+		{	static const bool m_Value = true;	};
+
+}
