e5bf460e3a04c111f251b70b475f08a30242fe33
Eduardo Madrigal martinez <emadriga@c1r2s6.42madrid.com>
2023-02-08 13:00:10 +0100

    use less possible friends

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 09:47:33 by emadriga          #+#    #+#             */
-/*   Updated: 2023/02/07 14:43:13 by emadriga         ###   ########.fr       */
+/*   Updated: 2023/02/08 12:53:58 by emadriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,77 +37,67 @@ namespace ft
 			container_type	m_Container;
 
 		public:
-	///*	Contructors & destructors
-		explicit stack (const container_type& ctnr = container_type())
-			:	m_Container(ctnr)	{}
+		///*	Contructors & destructors
+			explicit stack (const container_type& ctnr = container_type())
+				:	m_Container(ctnr)	{}
 
-		stack( const stack& copy )
-			{	operator=(copy);	}
+			stack( const stack& copy )
+				{	operator=(copy);	}
 
-		~stack(){}
+			~stack(){}
 
-		stack& operator=( const stack& assign )
-		{
-			if (this != &assign)
-				m_Container(assign.m_Container);
-			return *this;
-		}
+			stack& operator=( const stack& assign )
+			{
+				if (this != &assign)
+					m_Container(assign.m_Container);
+				return *this;
+			}
 
-	///*	Element access
-		reference top()	{	return m_Container.back();	}
-		const_reference top() const	{	return m_Container.back();	}
+		///*	Element access
+			reference top()	{	return m_Container.back();	}
+			const_reference top() const	{	return m_Container.back();	}
 
-	///*	Capacity
-		bool empty() const	{	return m_Container.empty();	}
-		size_type size() const	{	return m_Container.size();	}
+		///*	Capacity
+			bool empty() const	{	return m_Container.empty();	}
+			size_type size() const	{	return m_Container.size();	}
 
-	///*	Modifiers
-		void push( const value_type& value )	{	m_Container.push_back(value);	}
-		void pop()	{	m_Container.pop_back();	}
+		///*	Modifiers
+			void push( const value_type& value )	{	m_Container.push_back(value);	}
+			void pop()	{	m_Container.pop_back();	}
 
-		private:
+		///*	Friends (grants a function or another class access to private and protected members)
 			template <typename U, typename Cntr>
 			friend bool operator==(const stack<U, Cntr>& a, const stack<U, Cntr>& b);
-
-			template <typename U, typename Cntr>
-			friend bool operator!=(const stack<U, Cntr>& a, const stack<U, Cntr>& b);
-
 			template <typename U, typename Cntr>
 			friend bool operator<(const stack<U, Cntr>& a, const stack<U, Cntr>& b);
 
-			template <typename U, typename Cntr>
-			friend bool operator<=(const stack<U, Cntr>& a, const stack<U, Cntr>& b);
-
-			template <typename U, typename Cntr>
-			friend bool operator>(const stack<U, Cntr>& a, const stack<U, Cntr>& b);
-
-			template <typename U, typename Cntr>
-			friend bool operator>=(const stack<U, Cntr>& a, const stack<U, Cntr>& b);
+		private:	
+			container_type _getStack() const {	return m_Container; }
 	};
 
 	template <typename T, typename Container>
 	bool operator== (const stack<T,Container>& a, const stack<T,Container>& b)
-		{	return	a.m_Container == b.m_Container;	}
+		{	return	a._getStack() == b._getStack();	}
 
 	template <typename T, typename Container>
 	bool operator!= (const stack<T,Container>& a, const stack<T,Container>& b)
-		{	return	a.m_Container != b.m_Container;	}
+		{	return	!(a == b);	}
 
 	template <typename T, typename Container>
 	bool operator<  (const stack<T,Container>& a, const stack<T,Container>& b)
-		{	return	a.m_Container < b.m_Container;	}
+		{	return	a._getStack() < b._getStack();	}
 
 	template <typename T, typename Container>
 	bool operator<= (const stack<T,Container>& a, const stack<T,Container>& b)
-		{	return	a.m_Container <= b.m_Container;	}
+		{	return	!(b < a);	}
 
 	template <typename T, typename Container>
 	bool operator>  (const stack<T,Container>& a, const stack<T,Container>& b)
-		{	return	a.m_Container > b.m_Container;	}
+		{	return	(b < a);	}
 
 	template <typename T, typename Container>
 	bool operator>= (const stack<T,Container>& a, const stack<T,Container>& b)
-		{	return	a.m_Container >= b.m_Container;	}
+		{	return	!(a < b);	}
 
 
 }//namespace ft
