572c372ca4535a8069699e46c0fdc38aa82abe35
eM4dri <edu.mail.delivery+github@gmail.com>
2023-03-07 10:55:11 +0100

    improve test

--- test.bash
+++ test.bash
@@ -4,49 +4,53 @@ GREEN_COLOR='\033[0;32m'
 YELLOW_COLOR='\033[0;33m'
 RESET_COLOR='\033[0m'
 FILE=containers
+LOG_FILE=logs
+# remove previous outputs
+rm -rf containers.std containers.ft ft_out std_out logs
 
-echo -e "${YELLOW_COLOR}Compiling with std... Wait a sec.${RESET_COLOR}"
-make std > /dev/null
-if [ ! -f "${FILE}" ]
-then
-    echo -e "${RED}KO: Compilation error${RESET_COLOR}"
-    make std
-else
-    echo -e "${GREEN_COLOR}${FILE} generated!${RESET_COLOR}"
-    echo -e "mv containers containersSTD"
-    mv containers containersSTD
+mkdir logs
 
-    echo -e "${YELLOW_COLOR}Compiling with ft... Wait a sec.${RESET_COLOR}"
-    make ft > /dev/null
+namespaces="std ft"
+for ns in $namespaces
+do
+    echo -e "${YELLOW_COLOR}Compiling with $ns... Wait a sec.${RESET_COLOR}"
+    make $ns > /dev/null
     if [ ! -f "${FILE}" ]
     then
-        echo -e "${RED}KO: Compilation error${RESET_COLOR}"
-         make ft
+        echo -e "${RED}KO: Compilation error${RESET_COLOR} log in file logs/ft.$FILE.$ns.log"
+        make $ns > logs/ft.$FILE.$ns.log
     else
         echo -e "${GREEN_COLOR}${FILE} generated!${RESET_COLOR}"
-        echo -e "mv containers containersFT"
-        mv containers containersFT
-
-        echo -e "\nCompare outputs & timing (you can be up to 20 times slower)."
-        tests="vector stack map set utility"
-        for test in $tests
-        do
-            echo -e "\n    ./containers $test"
-            TIMEFORMAT='    -   ft took %3R seconds.'
-            time ./containersFT $test > ft_out
-            TIMEFORMAT='    -   std took %3R seconds.'
-            time ./containersSTD $test > std_out
-            DIFF=$(diff ft_out std_out)
-            if [ "${DIFF}" != "" ]
-            then
-                echo -e "     DIFF: ${RED_COLOR}KO${RESET_COLOR}\tlog in file ft.$test.log"
-                diff ft_out std_out > ft.$test.log
-            else
-                echo -e "      DIFF: ${GREEN_COLOR}OK${RESET_COLOR}"
-            fi
-        done
+        echo -e "mv $FILE $FILE.$ns"
+        mv $FILE $FILE.$ns
     fi
-fi
+done
 
-rm -f containersSTD containersFT ft_out std_out
+if [ -f "${FILE}.ft" ]
+then
+    echo -e "\nCompare outputs & timing (you can be up to 20 times slower)."
+    tests="vector stack map set utility"
+    for test in $tests
+    do
+        echo -e "\n    ./containers $test"
+        TIMEFORMAT='    -   ft took %3R seconds.'
+        time ./containers.ft $test > ft_out
+        TIMEFORMAT='    -   std took %3R seconds.'
+        time ./containers.std $test > std_out
+        DIFF=$(diff ft_out std_out)
+        if [ "${DIFF}" != "" ]
+        then
+            echo -e "     DIFF: ${RED_COLOR}KO${RESET_COLOR}\tlog in file logs/ft.$test.log"
+            diff ft_out std_out > logs/ft.$test.log
+        else
+            echo -e "      DIFF: ${GREEN_COLOR}OK${RESET_COLOR}"
+        fi
+    done
+fi
+LOGS_COUNT=$(ls -l ${LOG_FILE} | wc -l)
+# if  no error remove outputs
+if [ "${LOGS_COUNT}" > 1 ]
+then
+    rm -rf containers.std containers.ft ft_out std_out logs
+fi
 
